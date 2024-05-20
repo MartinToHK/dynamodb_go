ac83bfdf7dbdaa74c5b1a21a53c4a52958a2f5b0
Martin To <martin.to@baguio.com.hk>
2024-05-20 17:05:23 +0800

    update

--- internal/controllers/product.go
+++ internal/controllers/product.go
@@ -8,7 +8,7 @@ type Controller struct{
 }
 
 type Interface interface{
-	ListOne(id uuid.UUID) (entity product.Product, err error)
+	ListOne(ID uuid.UUID) (entity product.Product, err error)
 	ListAll()(entites []product.Product, err error)
 	Create(entity product.Product)(uuid.UUID, error)
 	Update(ID uuid.UUID, entity *product.Product) error
@@ -33,18 +33,65 @@ func (c *Controller) ListOne(id uuid.UUID) (entity product.Product, err error){
 }
 
 func (c *Controller) ListAll()(entites []product.Product, err error){
+	entities = []product.Product{}
+	var entity product.Product
+
+	filter := expression.Name("name").NotEqual(expression.Value(""))
+
+	condition, err := expression.NewBuilder().WithFilter(filter).Build()
+
+	if err != nil {
+		return entities, err
+	}
+
+	response, err := c.repository.FindAll(condition, entity.TableName())
+
+	if err != nil {
+		return entities, err
+	}
+
+	if response != nil {
+		for _, value : range response.Items{
+			entity, err := product.ParseDynamoAttributeToStruct(value)
+			if err != nil {
+				return entities, err
+			}
+			entities = append(entities, entity)
+		}
+	}
+	return entities, nil
 
 }
 
 func (c *Controller) Create(entity product.Product)(uuid.UUID, error){
+	entity.CreatedAt = time.Now()
+	c.repository.CreateOrUpdate(entity.GetMap(), entity.TableName())
+	return entity.ID, err
 
 }
 
 func (c *Controller) Update(id uuid.UUID, entity *product.Product) {
+	found, err := c.ListOne(id)
+	if err != nil {
+		return err
+	}
+
+	found.ID = id
+	found.Name = entity.Name
+	found.UpdatedAt = time.Now()
+	_, err = c.repository.CreateOrUpdate(found.GetMap(), entity.TableName())
+	return err
+
 
 }
 
 func (c *Controller) Remove(id uuid.UUID) error{
+	entity, err := c.ListOne(id)
+	if err != nil {
+		return err
+	}
 
+	_, err = c.repository.Delete(entity.GetFilterId(), entity.TableName())
+	return err
 }
 
